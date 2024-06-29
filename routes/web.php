665b5a322331c9cd8d38ccef47ddedf0d7a80ea3
stiven92cc <stivenciro1992@gmail.com>
2024-06-29 12:24:02 -0500

    wip

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,31 @@
 
 use App\Http\Controllers\Admin\Category\CategoryController;
 use App\Http\Controllers\Admin\Microsites\MicrositesController;
+use App\Http\Controllers\Admin\Roles\RolesController;
+use App\Http\Controllers\Admin\Users\UsersController;
+use App\Http\Controllers\Guest\WelcomeController;
+use App\Http\Controllers\Payment\PaymentController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome');
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
+
+Route::get('/payment/{microsite}', [PaymentController::class, 'micrositeForm'])->name('payment.form');
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::middleware('auth')->group(function () {
     Route::resource('microsites', MicrositesController::class);
+    Route::resource('categories', CategoryController::class);
+    Route::resource('users', UsersController::class);
+    Route::resource('roles', RolesController::class);
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('categories', CategoryController::class);
-Route::resource('microsites', \App\Http\Controllers\Admin\Microsites\MicrositesController::class);
 require __DIR__.'/auth.php';
