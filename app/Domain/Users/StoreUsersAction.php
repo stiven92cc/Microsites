266706b9c04b69f5bdee1b6ce3c48f5d3170d66d
stiven92cc <stivenciro1992@gmail.com>
2024-06-29 12:07:57 -0500

    feat: add user model and controller

---
+++ app/Domain/Users/StoreUsersAction.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Domain\Users;
+
+use App\Http\Requests\Microsites\StoreMicrositeRequest;
+use App\Http\Requests\Users\StoreUserRequest;
+use App\Infrastructure\Persistence\Models\Microsite;
+use App\Infrastructure\Persistence\Models\User;
+
+class StoreUsersAction
+{
+    public function execute(StoreUserRequest $request): void
+    {
+        User::query()->create($request->validated());
+    }
+}
