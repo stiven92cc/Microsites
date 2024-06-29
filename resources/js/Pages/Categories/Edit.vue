68bc1f6e895124b9fd54d6ad31f7e22660651e03
stiven92cc <stivenciro1992@gmail.com>
2024-06-29 12:14:20 -0500

    feat: add categories components

---
+++ resources/js/Pages/Categories/Edit.vue
@@ -0,0 +1,50 @@
+<template>
+    <!-- This is an example component -->
+    <AuthenticatedLayout>
+        <div class="my-8 max-w-md mx-auto p-8 bg-white rounded-md shadow-md">
+            <h2 class="text-2xl font-semibold mb-6">Category</h2>
+            <form @submit.prevent="submit">
+                <div class="mb-4">
+                    <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Name</label>
+                    <input type="text" id="name" name="name" v-model="form.name" required
+                           class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500">
+                </div>
+                <div class="mb-4">
+                    <label for="alias" class="block text-gray-700 text-sm font-bold mb-2">Alias</label>
+                    <input type="text" id="alias" name="alias" v-model="form.alias" required
+                           class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500">
+                </div>
+                <div class="mb-6">
+                    <label for="description" class="block text-gray-700 text-sm font-bold mb-2">Description</label>
+                    <textarea id="description" name="description" v-model="form.description" rows="4"
+                              class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"></textarea>
+                </div>
+                <button type="submit"
+                        class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue">
+                    Edit
+                </button>
+            </form>
+        </div>
+    </AuthenticatedLayout>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useForm } from '@inertiajs/vue3';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    category: Object
+});
+
+const form = useForm({
+    name: props.category.name,
+    description: props.category.description,
+    alias: props.category.alias
+});
+
+const submit = () => {
+    form.patch(route('categories.update', props.category.id));
+};
+</script>
---
+++ resources/js/Pages/Categories/Index.vue
@@ -0,0 +1,55 @@
+<template>
+    <AuthenticatedLayout>
+        <template #header>
+            <div class="flex">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">Categorias</h2>
+            </div>
+        </template>
+
+        <div class="flex justify-end w-full">
+            <Button class="mx-8 my-2" routeName="categories.create" text="Create" />
+        </div>
+
+        <DataTable :columns="columns" :rows="categories" />
+    </AuthenticatedLayout>
+</template>
+
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import DataTable from "@/Components/Molecules/DataTable.vue";
+import Button from "@/Components/Atoms/Button.vue";
+import { h } from 'vue';
+import { router } from '@inertiajs/vue3';
+import {PencilIcon, TrashIcon} from "@heroicons/vue/24/outline/index.js";
+
+defineProps({ categories: Array });
+
+const columns = [
+    { key: 'id', label: '#' },
+    { key: 'name', label: 'Name' },
+    { key: 'description', label: 'Description' },
+    { key: 'alias', label: 'Alias' },
+    { key: 'created_at', label: 'Created At' },
+    {
+        key: 'actions',
+        label: 'Actions',
+        formatter: (value, row) => ({
+            render() {
+                return h('div', {
+                    class: 'flex'
+                }, [
+                    h(PencilIcon,{
+                        onClick: () => router.get(`/categories/${row.id}/edit`),
+                        class: 'h-5 w-5 text-gray-400 hover:text-gray-800 mr-2 cursor-pointer',
+                    }),
+                    h(TrashIcon,{
+                        onClick: () => router.delete(`/categories/${row.id}`),
+                        class: 'h-5 w-5 text-gray-400 hover:text-red-500 mr-2 cursor-pointer',
+                    })
+                ])
+            }
+        })
+    },
+
+];
+</script>
