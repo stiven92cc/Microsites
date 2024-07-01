e92627dacd8dd9c5f233a6f1ed31ebf1a158abbb
stiven92cc <stivenciro1992@gmail.com>
2024-06-30 23:03:40 -0500

    feat: add necessary config for can use libraries of management access control for users

--- app/Infrastructure/Persistence/Models/User.php
+++ app/Infrastructure/Persistence/Models/User.php
@@ -5,11 +5,12 @@ namespace App\Infrastructure\Persistence\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
+use LaravelAndVueJS\Traits\LaravelPermissionToVueJS;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable, HasRoles, LaravelPermissionToVueJS;
 
 
     protected $fillable = [
--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import LaravelPermissionToVueJS from 'laravel-permission-to-vuejs';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -14,6 +15,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(LaravelPermissionToVueJS)
             .mount(el);
     },
     progress: {
