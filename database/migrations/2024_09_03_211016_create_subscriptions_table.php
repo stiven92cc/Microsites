4b508b411d10e243e83cfbf0473bd907447319e0
stiven92cc <stivenciro1992@gmail.com>
2024-09-08 23:19:47 -0500

    feat: add subscription migrate

--- database/migrations/2024_09_03_211016_create_subscriptions_table.php
+++ database/migrations/2024_09_03_211016_create_subscriptions_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Constants\PaymentStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,13 @@ return new class extends Migration
     {
         Schema::create('subscriptions', function (Blueprint $table) {
             $table->id();
+            $table->text('token')->nullable();
+            $table->text('sub_token')->nullable();
+            $table->text('lastDigits')->nullable();
+            $table->date('validUntil')->nullable();
+            $table->enum('status', PaymentStatus::getPaymentStatus())->nullable();
+            $table->foreignId('payment_id')->constrained()->onDelete('cascade');
+            $table->foreignId('subscription_plan_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
